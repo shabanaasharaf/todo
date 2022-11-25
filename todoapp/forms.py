15db78a25d2cb8503b324ee4db71a9649f0f2d8a
shabanaasharaf <shabanacn44@gmailcom>
2022-11-25 17:23:01 +0530

    success

---
+++ todoapp/forms.py
@@ -0,0 +1,6 @@
+from .models import Task
+from django import forms
+class ToDoForm(forms.ModelForm):
+    class Meta:
+        model=Task
+        fields=['name','priority','date']
---
+++ todoapp/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from todoapp import views
+
+urlpatterns = [
+
+    path('',views.add,name='add'),
+    path('delete/<int:task_id>',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.Taskdetailview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.Taskupdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.Taskdeleteview.as_view(),name='cbvdelete')
+]
